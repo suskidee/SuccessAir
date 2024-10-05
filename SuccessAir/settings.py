5927592e29ed70026575356cd46c7300d3c18418
Masodi Success <109852676+suskidee@users.noreply.github.com>
2024-10-05 22:30:25 +0100

    added docker containerization

--- SuccessAir/settings.py
+++ SuccessAir/settings.py
@@ -87,10 +87,23 @@ WSGI_APPLICATION = 'SuccessAir.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv("NAME"),
+            'USER': os.getenv("USER"),
+            'PASSWORD': os.getenv("DB_PASSWORD"),
+            'HOST': os.getenv("HOST"),
+            'PORT': os.getenv("PORT"),
+
     }
 }
 
